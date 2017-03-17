c7a3382ba1ff8c1526ef5f55fba6cca54751cd53
aBlender <jasonlongle@gmail.com>
2017-03-17 03:24:39 -0700

    Added voice and spotify search

--- vr/audio.js
+++ vr/audio.js
@@ -8,77 +8,177 @@ AFRAME.registerComponent('click-to-play',{
     playing: {default: false}
   },
   init: function(){
-    this.el.sceneEl.addEventListener('click', function(){
-      console.log("CLICKED ON SCENE");
-      var analyser = document.querySelector('#analyser');
-      var playing = analyser.getAttribute('click-to-play').playing;
-      var textEl = document.querySelector('#statusText');
+    //var data = this.data;
+    //var playing = this.data.playing;
+    var el = this.el;
+    var data = this.data;
+    el.addEventListener('click', function(){
+      console.log("CLICKED ON ELEMENT");
+      //var analyser = document.querySelector('#analyser');
+      //var data = this.el.data;
+      var playing = el.getAttribute('click-to-play').playing;
+      console.log(playing);
+      var textEl = document.querySelector('#playText');
       var audioEl = document.querySelector('#song');
       if (playing == false){
-        textEl.setAttribute("value", "   Playing Song   ");
+        textEl.setAttribute("value", "PAUSE SONG");
         audioEl.play();
-        analyser.setAttribute('click-to-play','playing',true);
+        el.setAttribute('click-to-play','playing',true);
       }
       else{
-        textEl.setAttribute("value", "   Pausing Song   ");
+        textEl.setAttribute("value", "PLAY SONG");
         audioEl.pause();
-        analyser.setAttribute('click-to-play','playing',false);
+        el.setAttribute('click-to-play','playing',false);
       }
     });
   }
 });
 
-AFRAME.registerComponent('click-to-play-spotify', {
-  schema: {
-    playing: {default: false}
-  },
-  init: function () {
+AFRAME.registerComponent('click-to-listen',{
+  init: function(){
     // Set up speech recognition.
     annyang.addCommands({
-      'play *song': this.searchTrack.bind(this)
+      '*song': this.searchTrack.bind(this)
     });
+    annyang.pause();
 
-    this.el.sceneEl.addEventListener('click', function(){
-      console.log("CLICKED ON SCENE");
-      var analyser = document.querySelector('#analyser');
-      var playing = analyser.getAttribute('click-to-play-spotify').playing;
-      var textEl = document.querySelector('#statusText');
-      //var audioEl = document.querySelector('#song');
-      var audioEl;
-
-      annyang.start();
-
-      // Create audio element to point to Spotify preview URL.
-      audioEl = document.createElement('audio');
-      audioEl.crossOrigin = 'anonymous';
-      audioEl.loop = true;
-      audioEl.id = 'spotifyTrack';
-      analyser.appendChild(audioEl);
-      analyser.setAttribute('audioanalyser', {src: '#spotifyTrack'});
-
-      /*if (playing == false){
-        textEl.setAttribute("value", "   Playing Song   ");
-        audioEl.play();
-        analyser.setAttribute('click-to-play','playing',true);
-      }
-      else{
-        textEl.setAttribute("value", "   Pausing Song   ");
-        audioEl.pause();
-        analyser.setAttribute('click-to-play','playing',false);
-      }*/
+    this.el.addEventListener('click', function(){
+      console.log("Listening");
+      var textEl = document.querySelector('#songInfo');
+      var playText = document.querySelector('#playText')
+      var audioEl = document.querySelector('#song');
+      var playEl = document.querySelector('#rightEyeball');
+      playText.setAttribute("value", "PLAY SONG");
+      audioEl.pause();
+      playEl.setAttribute('click-to-play','playing',false);
+      textEl.setAttribute("value", "     Listening     ");
+      annyang.start({autoRestart: false, continuous: false});
     });
-
-
   },
   searchTrack: function (query) {
-    var audioEl = document.querySelector('#spotifyTrack');
+    var audioEl = document.querySelector('#song');
+    var textEl = document.querySelector('#songInfo');
+    textEl.setAttribute("value", "Searching for " + query);
     var el = this.el;
-    Spotify.searchTracks(query).then(function (results) {
-      var track = results.tracks.items[0];
-      var previewUrl = track.preview_url;
-      el.emit('spotify-play', results);
-      audioEl.src = track.preview_url;
-      audioEl.play();
-    });
+    console.log(query);
+    if (query == "trash80"){
+      audioEl.src = "trash80.mp3";
+      textEl.setAttribute("value", "Trash80 - Sidk Fnck");
+    }else if(query == "cider time"){
+      audioEl.src = "CiderTime.flac";
+      textEl.setAttribute("value", "Lifeformed - Cider Time");
+    }else{
+      Spotify.searchTracks(query).then(function (results) {
+        var track = results.tracks.items[0];
+        textEl.setAttribute("value", track.artists[0].name + " - " + track.name);
+        var previewUrl = track.preview_url;
+        el.emit('spotify-play', results);
+        audioEl.src = track.preview_url;
+      });
+    }
   }
+});
+
+AFRAME.registerSystem('speech-command', {
+    init: function () {
+        //console.log("in speech-command system init");
+        this.entities = [];
+        //window.addEventListener('loaded', this.onSceneLoaded.bind(this));
+    },
+    registerMe: function (comp) {
+        this.entities.push(comp);
+        //console.log("in register, comp: "+comp.data.command);
+    },
+    unregisterMe: function (comp) {
+        var index = this.entities.indexOf(comp);
+        this.entities.splice(index, 1);
+    },
+    onSceneLoaded: function(evt) {
+        //console.log("in speech-command system onSceneLoaded listener");
+    },
+    play: function() {
+        //console.log("in system play, entities: "+this.entities);
+    }
+});
+AFRAME.registerComponent('speech-command', {
+    multiple: true,
+    schema: {
+        command: { type: 'string' },
+        type: { type: 'string' },
+        targetElement: { type: 'selector' },
+        targetComponent: { type: 'string' },
+        function: { type: 'string' },
+        attribute: { type: 'string' },
+        value: { type: 'string' },
+        keyCode: { type: 'string' }
+    },
+    init: function () {
+        this.system.registerMe(this);
+        if (!this.data.targetElement) {
+            this.data.targetElement = this.el;
+        }
+        if (this.data.keyCode) {
+            window.addEventListener('keyup', this.onKeyup.bind(this));
+        }
+    },
+    remove: function () {
+        this.system.unregisterMe(this);
+    },
+    play: function() {
+        //console.log("in speech-command play, command: "+this.data.command+", type: "+this.data.type);
+    },
+    executeCommand: function () {
+        //console.log("in executeCommand for: "+this.data.targetElement);
+        var targetElement = this.data.targetElement;
+        if (this.data.type == 'attribute') {
+            //console.log("about to change attribute "+this.data.attribute+" to: "+this.data.value);
+            targetElement.setAttribute(this.data.attribute, this.data.value);
+        } else if (this.data.type == 'function') {
+            console.log("targetElement: "+targetElement+", components: "+ this.data.targetComponent);
+            if (this.data.targetComponent == ""){
+              targetElement[this.data.function]();
+            }else{
+              var targetComponent = targetElement.components[this.data.targetComponent];
+              targetComponent[this.data.function]();
+            }
+        }
+    },
+    onKeyup: function (evt) {
+        if (evt.keyCode == this.data.keyCode) {
+            //console.log("in speech command keyup for: "+this.data.command);
+            this.executeCommand();
+        }
+    }
+});
+AFRAME.registerComponent('annyang-speech-recognition', {
+    init: function () {
+        //console.log("in annyang-speech-recognition init");
+    },
+    play: function() {
+        if (annyang) {
+            //console.log("annyang: "+annyang);
+            console.log("annyang.addCommands: "+annyang.addCommands);
+            var speechCommandSystem = document.querySelector('a-scene').systems['speech-command'];
+            var commands = {};
+            var commandsMap = {};
+            for (var i = 0; i < speechCommandSystem.entities.length; i++) {
+                var speechCommand = speechCommandSystem.entities[i];
+                commandsMap[speechCommand.data.command] = speechCommand;
+                // note: function empty here because real work is done in the resultMatch callback below
+                commands[speechCommand.data.command] = function() { };
+            }
+            annyang.addCommands(commands);
+
+            annyang.addCallback('resultMatch', function(userSaid, commandText, phrases) {
+                //console.log("commandText: "+commandText); // sample output: 'hello (there)'
+                var speechCommand = commandsMap[commandText];
+                speechCommand.executeCommand();
+                annyang.pause();
+            });
+
+            // Start listening. You can call this here, or attach this call to an event, button, etc.
+            annyang.pause();
+        }
+    }
+
 });
